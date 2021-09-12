385839b41bbb7bb016cbcd4c3d2595647134a09a
Dan Radu <radudanielro@yahoo.com>
2021-09-12 11:55:35 +1000

    Added rose app K8S manifest

--- src/infra/script.sh
+++ src/infra/script.sh
@@ -7,9 +7,16 @@ az acr login -n $acrName
 
 docker images
 
+# push local image manually
 imageName='hello-rose-world'
 tag='0.0.1'
 destTag='0.0.1'
 $ns='geekready2021'
 docker tag $imageName:$tag $loginServer/$ns/$imageName:$destTag
 docker push $loginServer/$ns/$imageName:$destTag
+
+# attach ACR instance to AKS
+resourceGroupName='zz-dev'
+aksName='zz-dev-aks'
+acrName='zzacr'
+az aks update -n $aksName -g $resourceGroupName --attach-acr $acrName
